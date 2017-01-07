e30e3496a043d6df8bb349d43d08157ad21efd2f
David Q <davquang@gmail.com>
2017-01-07 03:23:46 +0000

    canvas

--- fluids/velocity.js
+++ fluids/velocity.js
@@ -2,45 +2,19 @@
 //plot U as contour as background image
 //plot points and loop
 
-var background = new Image();
-var u = [];
+var u = [];    
+var uu = [];
 var x = [];
 var y = [];
 
 function calculate(){
     var a = document.getElementById("mach").value;
     var b = document.getElementById("alpha").value;
-    
     calculateU(a, b);
-
-    /*var html = "<table><tr><td></td>";
-    for(var i = 0; i < 100; ++i)
-    {
-        html += "<td>" + i + "</td>";
-    }
-    html += "</tr>";
-    
-    for(var i = 0; i < 160; ++i)
-    {
-        html += "<tr><td>" + i + "</td>";
-        for(var j = 0; j < 100; ++j)
-        {
-            html += "<td>";
-            html += Number(u[i][j]).toFixed(6) + "    ";
-            html += "</td>";
-        }
-        html += "</tr>";
-    }
-    html += "</table>";*/
     document.getElementById("buttons").innerHTML = "<button id=\"display\" onclick=\"plot()\">Display</button>";
-    //document.getElementById("data").innerHTML = html;   
 }
 
-function plot()
-{
-
-    var uu = [];
-    
+function plot() {
     for(var j = 0; j < 100; ++j)
     {
         uu[j] = [];
@@ -82,16 +56,153 @@ function plot()
     };
     document.getElementById("buttons").innerHTML = "<button id=\"solve\" onclick=\"calculate()\">Calculate</button>";
     Plotly.newPlot("data", data, layout);
+	normalizeUU();
 }
 
-function displayU(){
+//~~~~~~~~~~~~~~~Begin Canvas Functions
 
+var points = [];
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+var count = [];
+var frameCount = 0;
+var run = false;
+
+function init() {
+    ctx.fillStyle = '#F6F6F6';
+    ctx.fillRect(0, 0, 800,500);
+
+	drawAirfoil();
+	createPoints();
+	
+    requestAnimationFrame(draw);
 }
 
-function drawBackground(){
-    
+function drawAirfoil() {
+	ctx.beginPath();
+	ctx.strokeStyle = '#00ff2b';
+	ctx.lineWidth = 2;
+	ctx.moveTo(5*40, 5*48);
+	ctx.lineTo(5*60, 5*48);
+	ctx.stroke();
 }
 
-function draw(){
-
+function normalizeUU() {
+	var min = 0;
+	var max = 0;
+	for(var j = 0; j < y.length; ++j) {
+		if(Math.min.apply(null, uu[j]) < min) {
+			min = Math.min.apply(null, uu[j]);
+		}
+		if(Math.max.apply(null, uu[j]) > max) {
+			max = Math.max.apply(null, uu[j]);
+		}
+	}
+	
+    for(var j = 0; j < y.length; ++j) {
+        for(var i = 0; i < x.length; ++i) {
+			uu[j][i] += Math.abs(min);
+			uu[j][i] += 0.015;
+			//normalize and scale to .25 speed
+			uu[j][i] = uu[j][i]/(max-min)*0.25;
+		}
+	}
 }
+
+function Point(pointX, pointY, pointU) {
+    this.pointX = pointX;
+    this.pointY = y.length - 1 - pointY;
+    this.pointU = pointU;
+    this.pointColor = "#FFFFFF";
+}
+
+Point.prototype.draw = function (ctx) {
+	ctx.fillStyle = "hsla(" + this.pointColor + ", 100%, 50%, 1.0)";
+	//ctx.fillStyle = "#FFFFFF";
+	ctx.fillRect(5*this.pointX, 5*this.pointY, 2, 2);
+}
+
+Point.prototype.update = function() {
+	//uu is currently mirrored horizontally across the middle
+	//Reflect y when updating velocities
+	this.pointU = uu[y.length - 1 - Math.floor(this.pointY)][Math.floor(this.pointX)];
+	this.pointX += this.pointU;
+	this.pointColor = 240 - this.pointU/0.25*360*2/3;
+	
+	/*
+	if(this.pointX > x.length) {
+		this.pointX = 0;
+		this.pointU = uu[y.length - 1 - Math.floor(this.pointY)][0];
+	}*/
+}
+
+function createPoints() {
+    for(var j = 0; j < y.length; j+=2) {
+		points[j/2] = [];
+        for(var i = 0; i < x.length; i+=2) {
+			points[j/2][i/2] = new Point(i, j, uu[j][i]);
+			points[j/2][i/2].draw(ctx);
+        }
+    }
+}
+
+function resetColumn(col) {
+	for(var j = 0; j < points.length; ++j) {
+		points[j][col].pointX = 0;
+		points[j][col].pointU = uu[y.length - 1 - Math.floor(points[j][col].pointY)][0];
+	}
+}
+
+canvas.addEventListener("click", function(e) {
+	if(points.length === 0) {
+		init();
+	}
+	run = !run;
+});
+
+function draw() {
+	if(run == true) {
+	    ctx.clearRec
+	    ctx.fillStyle = '#000000';
+	    ctx.fillRect(0, 0, 800,500);
+	
+		//draw airfoil
+		drawAirfoil();
+		
+		for(var i = 0; i < points[0].length; ++i) {
+			count[i] = 0;
+	    }
+		
+		//update particle positions and draw
+	    for(var j = 0; j < points.length; j++) {
+	        for(var i = 0; i < points[j].length; i++) {
+				//update position
+				points[j][i].update();
+				//determine and count updated points out of bounds
+				if(isNaN(points[j][i].pointX) && i > 80) {
+					points[j].pop();
+					/*
+					count[i] += 1;
+					//if all points in a column are OoB, move back to the left
+					if(count[i] === points.length) {
+						resetColumn(i);
+						count[i] = 0;
+					}
+					*/
+				} else {
+	            	points[j][i].draw(ctx);
+				}
+	        }
+	    }
+		
+		if(frameCount % 20 === 0) {
+			for(var j = 0; j < points.length; ++j) {
+				points[j].unshift(new Point(0, 2*j, uu[j][0]));
+			}
+			frameCount = 0;
+		}
+		++frameCount;
+	}
+	requestAnimationFrame(draw);
+}
+
